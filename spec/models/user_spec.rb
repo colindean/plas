95b616d2767fe58d7f874f1d1e0956fb53716e16
Colin Dean <cdean@pittco.org>
2010-09-07 23:28:35 -0400

    added some stub tests for users so others can fill

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,30 @@ describe User do
 		g = User.new
 		g.can('').should == true
 	end	
+	it "should have one user after creating a user" do
+		User.new
+		User.all.count.should == 1
+	end
+	it "should yield the user when accessed" do
+		u = User.new
+		u = User.all[0]
+		u.class.should == "User"
+	end
+	it "should have the same info we set in the factory" do
+		#create a user
+		u = User.new
+		u = User.all[0]
+		u.email.should == "email@pittco.org"
+	end
+	it "should fail to create the user with a bad email" do
+		u = User.new #set the email to be something failure
+		u.save!.should == false
+	end
+	it "should have one user after destroying a user" do
+		#create a user
+		u = User.new
+		User.all.count.should == 1
+		u.destroy
+		User.all.count.should == 0
+	end
 end
