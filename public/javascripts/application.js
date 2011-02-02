5b659d12d761ade2b6f85fccf5d743a189157cc1
Colin Dean <cad@cad.cx>
2011-02-01 23:05:38 -0500

    clock now changes per second

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,5 +2,12 @@
 // This file is automatically included by javascript_include_tag :defaults
 $(function(){
   $('p.notice').delay(2000).fadeOut(2000);
+
+  setInterval("clock_tick()", 1000);
 });
 
+function clock_tick(){
+  var e = $('#current-time');
+  var d = Date.now();
+  e.text(d.toString('yyyy-MM-dd HH:mm:ss'));
+}
