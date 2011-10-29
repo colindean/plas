b6f521f6446b92cf9ea9309bf1afaf9b90e13c8c
Colin Dean <git@cad.cx>
2011-10-28 23:00:53 -0400

    add some validations and a helper method

--- app/models/remote_tournament.rb
+++ app/models/remote_tournament.rb
@@ -1,14 +1,37 @@
+=begin
+  
+This class exists primarily to act as a bridge between a tournament and its
+remote API interface, if one exists.
+  
+=end
 class RemoteTournament < ActiveRecord::Base
   belongs_to :tournament, :polymorphic => true
   
-  #tournament, #remote_type, #remote_id
+  validates :remote_type, :presence => :true
+  validates :remote_id, :presence => :true
+  
+  #tournament, #remote_type, #remote_id 
+  
+  def type=(c)
+    raise Error if c.class != Class
+    remote_type = c.to_s
+  end 
+  
+  def type
+    resolve_class
+  end
   
   def resolve_class
     remote_type.to_s.classify.constantize
   end
   
   def get
-      resolve_class.find(remote_id)  
+    resolve_class.find(remote_id)  
   end
   
+  def set(remote_instance)
+    self.type = remote_instance.class
+    self.remote_id = remote_instance.id
+    get
+  end
 end
--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,8 +1,12 @@
 class Tournament < ActiveRecord::Base
   belongs_to :event
   has_and_belongs_to_many :participants, :class_name => "User"
-           
   has_one :remote_tournament, :dependent => :destroy
+  
+  validates :name, :presence => true, :uniqueness => true, :length => {:minimum => 2 }
+  validates :max_participants, :numericality => { :greater_than => 0 }
+  validates :event, :presence => true
+
 
   #name, description, max_participants, 
   #started_at, start_time, 
