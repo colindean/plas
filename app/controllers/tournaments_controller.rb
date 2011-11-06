d67001203b539aaada08138d974167e0d6239411
Colin Dean <cdean@pittco.org>
2011-11-05 20:40:49 -0400

    add CRUD stuff to tournament controller
    I don't know why it wasn't there -- I apparently didn't create it with scaffold

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -24,6 +24,65 @@ class TournamentsController < ApplicationController
       flash[:alert] = _("Unable to retrieve tournaments from the Challonge server: " + err.message)
     end
   end
+
+  #GET /event/:id/tournament/new
+  #GET /event/:id/tournament/new.xml
+  def new
+    @tournament = Tournament.new
+    
+    respond_to do |format|
+      format.html
+      format.xml { render :xml => @tournament }
+    end
+  end
+
+  #GET /event/1/tournament/1/edit
+  def edit
+    @tournament = Tournament.find params[:id]
+  end
+
+  #POST /event/1/tournament
+  #POST /event/1/tournament.xml
+  def create
+    @tournament = Tournament.new params[:tournament]
+
+    respond_to do |format|
+      if @tournament.save
+        format.html { redirect_to(@tournament, :notice => _("Tournament was successfully created.")) }
+        format.xml { render :xml => @tournament, :status => :created, :location => @tournament }
+      else
+        format.html { render :action => "new" }
+        format.xml { render :xml => @tournamet.errors, :status => :unprocessable_entity  }
+      end
+    end
+  end
+
+  def update
+    @tournament = Tournament.find params[:id]
+
+    respond_to do |format|
+      if @tournament.update_attributes params[:tournament]
+        format.html { redirect_to(@tournament, :notice => _("Tournament was successfully updated.")) }
+        format.xml { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml { render :xml => @tournament.errors, :status => :unprocessable_entity  }
+      end
+    end
+  end
+
+  def destroy
+    @tournament = Tournament.find params[:id]
+    @tournament.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(tournaments_url) }
+      format.html { head :ok }
+    end
+  end
+
+
+
   
   private
   
